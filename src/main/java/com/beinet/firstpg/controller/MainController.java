52e625c974d8bc27d0d2ae82e1c9fd716c89333c
fankuai <fankuai@mikeo2o.com>
2019-05-17 17:30:24 +0800

    Web服务Demo增加： 1、上传Demo； 2、读取请求Header和写入响应Header的Demo。

--- src/main/java/com/beinet/firstpg/controller/MainController.java
+++ src/main/java/com/beinet/firstpg/controller/MainController.java
@@ -2,12 +2,24 @@ package com.beinet.firstpg.controller;
 
 import com.beinet.firstpg.mysql.MySqlService;
 import com.beinet.firstpg.mysql.entity.Users;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
 import java.time.LocalDateTime;
+import java.util.Enumeration;
 import java.util.List;
 
 @RestController
@@ -15,6 +27,8 @@ import java.util.List;
 public class MainController {
     @Autowired
     MySqlService mySqlTest;
+
+    // <editor-fold desc="常规api接口">
 
     /**
      * http://localhost:8081/
@@ -49,4 +63,87 @@ public class MainController {
     public List<Users> GetUsersWeek(){
         return mySqlTest.GetUsersByWeek();
     }
+
+
+    // </editor-fold>
+
+
+    // <editor-fold desc="请求Header和响应Header操作Demo">
+
+    /**
+     * 演示往输出里添加Header，cookie加起来麻烦。
+     * @return
+     */
+    @GetMapping("header")
+    public ResponseEntity TestHeader(){
+        HttpHeaders headers = new HttpHeaders();
+        // 下面2种方式都可以写入Header的 Location
+        headers.add("Location", "http://www.baidu.com/");
+        // headers.setLocation(URI.create("Http://www.beinet.cn/"));
+        // 添加自定义Header
+        headers.add("abcde", LocalDateTime.now().toString());
+        headers.add("Set-Cookie", "def1=2dd325; Max-Age=12; Expires=Fri, 27-May-2019 09:10:54 GMT; HttpOnly");
+        headers.add("Set-Cookie", "abc1=2325");
+        ResponseEntity ret = new ResponseEntity( "234", headers, HttpStatus.OK);
+        return ret;
+    }
+
+
+    /**
+     * 演示往输出里添加Header的另一种方法
+     * @return
+     */
+    @GetMapping("header2")
+    public String TestHeader2(HttpServletResponse response){
+        response.addCookie(new Cookie("abc", "2325"));
+
+        Cookie cook = new Cookie("def", "2dd325");
+        cook.setHttpOnly(true);
+        cook.setMaxAge(3600); // 单位秒
+        cook.setComment("dfsdf");
+        response.addCookie(cook);
+        response.addHeader("dsaasd", LocalDateTime.now().toString());
+        return "header222";
+    }
+
+
+    /**
+     * 演示读取请求信息
+     * @return
+     */
+    @GetMapping("request")
+    public ResponseEntity TestRequest(HttpServletRequest request){
+        // String ua = request.getHeader("user-agENT"); // key不区分大小写
+
+        StringBuilder sb = new StringBuilder();
+        // 获取所有请求Header
+        Enumeration<String> headers = request.getHeaderNames();
+        while(headers.hasMoreElements()){
+            String name = headers.nextElement();
+            sb.append(name).append("===").append(request.getHeader(name)).append("<br>");
+        }
+
+        sb.append("<hr>");
+        sb.append(request.getRequestURI()).append("<br>").append(request.getQueryString()).append("<br>");
+        sb.append(request.getRemoteAddr());
+
+        ResponseEntity ret = new ResponseEntity(sb, null, HttpStatus.OK);
+        return ret;
+    }
+
+    // </editor-fold>
+
+
+    // <editor-fold desc="文件上传Demo">
+    @PostMapping("upload")
+    public String upload(@RequestParam("file")MultipartFile file) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream("d:/tmp.tmp")) {
+            try (InputStream is = file.getInputStream()) {
+                IOUtils.copy(is, fos);
+            }
+        }
+        return file.getOriginalFilename() + "上传成功";
+    }
+    // </editor-fold>
+
 }

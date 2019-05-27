d429236700dd482ab9ab9dc31d797a7290f13186
fankuai <fankuai@mikeo2o.com>
2019-05-27 11:12:08 +0800

    添加不依赖Spring初始化的Redis连接。

--- src/main/java/com/beinet/firstpg/redisDemo/RedisHelper.java
+++ src/main/java/com/beinet/firstpg/redisDemo/RedisHelper.java
@@ -1,17 +1,21 @@
 package com.beinet.firstpg.redisDemo;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.parser.ParserConfig;
-import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.beinet.firstpg.configs.ConfigHelper;
+import com.beinet.firstpg.serializeDemo.SerializeHelper;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.Charset;
 import java.time.Duration;
 import java.util.concurrent.Callable;
 
@@ -19,25 +23,20 @@ import java.util.concurrent.Callable;
 public class RedisHelper {
     static RedisTemplate redis;
 
+    static {
+        RedisHelper.redis = initConnection();
+    }
+
     /**
      * 构造函数进行静态注入
+     *
      * @param redis1 注入对象
      */
     @Autowired
-    public RedisHelper(@Qualifier("redisTemplate") RedisTemplate redis1){
-        // Key用StringRedisSerializer，避免写入Redis的Key和Value，前缀都会出现 \xAC\xED\x00\x05t\x00\x03
-        RedisSerializer keySerializer = new StringRedisSerializer();
-        redis1.setKeySerializer(keySerializer);
-        redis1.setHashKeySerializer(keySerializer);
-
-        RedisSerializer valSerializer = new FastJsonRedisSerializer<>(Object.class);
-        redis1.setValueSerializer(valSerializer);
-        redis1.setHashValueSerializer(valSerializer);
-
-        // 反序列化支持需要配置，否则会报错：autoType is not support.
-        // 因为有过安全漏洞，不建议全局开启AutoType，https://www.jianshu.com/p/a92ecc33fd0d
-        // ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
-        ParserConfig.getGlobalInstance().addAccept("com.beinet.");
+    public RedisHelper(@Qualifier("redisTemplate") RedisTemplate redis1) {
+        if (redis1 == null || redis != null)
+            return;
+        initSerializer(redis1);
 
         RedisHelper.redis = redis1;
     }
@@ -46,10 +45,11 @@ public class RedisHelper {
 
     /**
      * 读取指定key
+     *
      * @param key Redis的key
      * @return 字符串
      */
-    public static String get(String key){
+    public static String get(String key) {
         Object ret = redis.opsForValue().get(key);
         if (ret == null) return null;
         return ret.toString();
@@ -57,61 +57,65 @@ public class RedisHelper {
 
     /**
      * 删除指定key
+     *
      * @param key Redis的key
      * @return 成败
      */
-    public static boolean remove(String key){
+    public static boolean remove(String key) {
         return redis.delete(key);
     }
 
 
-
     /**
      * 写入指定key, 存在时覆盖
+     *
      * @param key Redis的key
      * @param val Redis的val
      */
-    public static void set(String key, String val){
+    public static void set(String key, String val) {
         redis.opsForValue().set(key, val);
     }
 
 
     /**
      * 写入指定key, 存在时覆盖
-     * @param key Redis的key
-     * @param val Redis的val
+     *
+     * @param key         Redis的key
+     * @param val         Redis的val
      * @param cacheSecond 缓存时长，单位秒
      */
-    public static void set(String key, String val, int cacheSecond){
+    public static void set(String key, String val, int cacheSecond) {
         redis.opsForValue().set(key, val, Duration.ofSeconds(cacheSecond));
     }
 
 
-
     /**
      * 写入指定key, 存在时 不覆盖
+     *
      * @param key Redis的key
      * @param val Redis的val
      * @return 写入成败
      */
-    public static boolean add(String key, String val){
+    public static boolean add(String key, String val) {
         return redis.opsForValue().setIfAbsent(key, val);
     }
 
 
     /**
      * 写入指定key, 存在时 不覆盖
-     * @param key Redis的key
-     * @param val Redis的val
+     *
+     * @param key         Redis的key
+     * @param val         Redis的val
      * @param cacheSecond 缓存时长，单位秒
      * @return 写入成败
      */
-    public static boolean add(String key, String val, int cacheSecond){
+    public static boolean add(String key, String val, int cacheSecond) {
         return redis.opsForValue().setIfAbsent(key, val, Duration.ofSeconds(cacheSecond));
     }
 
     /**
      * 读取指定key的旧值返回, 并写入新数据
+     *
      * @param key Redis的key
      * @param val Redis的val
      * @return 字符串
@@ -126,15 +130,16 @@ public class RedisHelper {
 
     /**
      * 获取缓存，不存在时调用方法进行填充
-     * @param key 缓存key
-     * @param cacheSecond 缓存时长，单位秒
+     *
+     * @param key              缓存key
+     * @param cacheSecond      缓存时长，单位秒
      * @param fillWhileNoFound 缓存不存在时的数据获取方法，用于填充缓存
-     * @param cacheNull 数据获取方法返回null时，是否填充缓存
-     * @param <T> 缓存数据类型
+     * @param cacheNull        数据获取方法返回null时，是否填充缓存
+     * @param <T>              缓存数据类型
      * @return 缓存数据
      * @throws Exception 方法的异常
      */
-    public static<T> T getCache(String key, int cacheSecond, Callable fillWhileNoFound, boolean cacheNull) throws Exception {
+    public static <T> T getCache(String key, int cacheSecond, Callable fillWhileNoFound, boolean cacheNull) throws Exception {
         Object ret = redis.opsForValue().get(key);
         if (ret != null)
             return (T) ret;
@@ -149,8 +154,6 @@ public class RedisHelper {
 
     static class FastJsonRedisSerializer<T> implements RedisSerializer<T> {
 
-        public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
-
         private Class<T> clazz;
 
         public FastJsonRedisSerializer(Class<T> clazz) {
@@ -160,20 +163,73 @@ public class RedisHelper {
 
         @Override
         public byte[] serialize(T t) throws SerializationException {
-            if (t == null) {
-                return new byte[0];
-            }
-            return JSON.toJSONString(t, SerializerFeature.WriteClassName).getBytes(DEFAULT_CHARSET);
+            return SerializeHelper.serialize(t);
         }
 
         @Override
         public T deserialize(byte[] bytes) throws SerializationException {
-            if (bytes == null || bytes.length <= 0) {
-                return null;
-            }
-            String str = new String(bytes, DEFAULT_CHARSET);
-            return (T) JSON.parseObject(str, clazz);
+            return SerializeHelper.deserialize(bytes, clazz);
         }
 
+    }
+
+
+    public static RedisTemplate initConnection() {
+        // 单实例redis
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        // 哨兵redis 用 new RedisSentinelConfiguration();
+        // 集群redis 用 new RedisClusterConfiguration();
+
+        String host = ConfigHelper.getConfig("spring.redis.host");
+        String pwd = ConfigHelper.getConfig("spring.redis.password");
+        int port = ConfigHelper.getInt("spring.redis.port", 6379);
+        int db = ConfigHelper.getInt("spring.redis.database", 0);
+        int timeout = ConfigHelper.getInt("spring.redis.timeout", 1000);
+        boolean useSsl = ConfigHelper.getBool("spring.redis.ssl");
+
+        redisConfig.setHostName(host);
+        redisConfig.setPassword(RedisPassword.of(pwd));
+        redisConfig.setPort(port);
+        redisConfig.setDatabase(db);
+
+        int maxActive = ConfigHelper.getInt("spring.redis.jedis.pool.max-active", 8);
+        int maxWait = ConfigHelper.getInt("spring.redis.jedis.pool.max-wait", -1);
+        int maxIdel = ConfigHelper.getInt("spring.redis.jedis.pool.max-idle", 8);
+        int minIdel = ConfigHelper.getInt("spring.redis.jedis.pool.min-idle", 0);
+
+        GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+        poolConfig.setMaxTotal(maxActive);
+        poolConfig.setMaxIdle(maxIdel);
+        poolConfig.setMaxWaitMillis(maxWait);
+        poolConfig.setMinIdle(minIdel);
+
+        LettucePoolingClientConfiguration.LettucePoolingClientConfigurationBuilder builder = LettucePoolingClientConfiguration
+                .builder()
+                .poolConfig(poolConfig)
+                .commandTimeout(Duration.ofMillis(timeout));
+        if (useSsl)
+            builder.useSsl();
+        LettuceClientConfiguration config = builder.build();
+
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisConfig, config);
+        factory.afterPropertiesSet();
+
+        RedisTemplate redis = new RedisTemplate();
+        redis.setConnectionFactory(factory);
+        redis.afterPropertiesSet();
+
+        initSerializer(redis);
+        return redis;
+    }
+
+    private static void initSerializer(RedisTemplate redis1) {
+        // Key用StringRedisSerializer，避免写入Redis的Key和Value，前缀都会出现 \xAC\xED\x00\x05t\x00\x03
+        RedisSerializer keySerializer = new StringRedisSerializer();
+        redis1.setKeySerializer(keySerializer);
+        redis1.setHashKeySerializer(keySerializer);
+
+        RedisSerializer valSerializer = new FastJsonRedisSerializer<>(Object.class);
+        redis1.setValueSerializer(valSerializer);
+        redis1.setHashValueSerializer(valSerializer);
     }
 }
--- src/test/java/com/beinet/firstpg/redisTest/RedisTest.java
+++ src/test/java/com/beinet/firstpg/redisTest/RedisTest.java
@@ -16,39 +16,14 @@ import java.time.LocalDateTime;
 // RedisHelper要注入，所以要用 SpringBootTest
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class RedisTest  extends BaseTest {
+public class RedisTest  extends RedisTestNoSpring {
 
     /**
      * 测试正常的Redis读写（永不过期）
      */
     @Test
     public void TestStr(){
-        String key1 = "key1";
-        String key2 = "key2";
-
-        boolean bool = RedisHelper.remove(key1);
-        out(bool);
-        bool = RedisHelper.remove(key2);
-        out(bool);
-
-        String ret = RedisHelper.get(key1);
-        Assert.assertEquals(ret, null);
-        out(ret);
-
-        ret = RedisHelper.getAndSet(key1, "123");
-        Assert.assertEquals(ret, null);
-        out(ret);
-
-        ret = RedisHelper.getAndSet(key1, "456");
-        Assert.assertEquals(ret, "123");
-        out(ret);
-
-        bool = RedisHelper.add(key1, "789");
-        Assert.assertEquals(bool, false);
-        out(bool);
-        bool = RedisHelper.add(key2, "789");
-        Assert.assertEquals(bool, true);
-        out(bool);
+        super.TestStr();
     }
 
 
@@ -57,68 +32,12 @@ public class RedisTest  extends BaseTest {
      */
     @Test
     public void TestStrExpired() throws InterruptedException {
-        String key1 = "key1";
-        String key2 = "key2";
-
-        boolean bool = RedisHelper.remove(key1);
-        out(bool);
-        bool = RedisHelper.remove(key2);
-        out(bool);
-
-        String ret = RedisHelper.get(key1);
-        Assert.assertEquals(ret, null);
-        out(ret);
-
-        RedisHelper.set(key1, "123", 5);
-        ret = RedisHelper.get(key1);
-        Assert.assertEquals(ret, "123");
-        out(ret);
-        Thread.sleep(5000);
-        ret = RedisHelper.get(key1);
-        Assert.assertEquals(ret, null);
-        out(ret);
-
-        bool = RedisHelper.add(key1, "123", 5);
-        Assert.assertEquals(bool, true);
-        out(bool);
-        ret = RedisHelper.get(key1);
-        Assert.assertEquals(ret, "123");
-        out(ret);
-        Thread.sleep(5000);
-        ret = RedisHelper.get(key1);
-        Assert.assertEquals(ret, null);
-        out(ret);
-
+        super.TestStrExpired();
     }
 
 
     @Test
     public void TestCache() throws Exception {
-        String key = "abc";
-        String name = "xxx";
-        for (int i = 0; i < 7; i++) {
-            Emp emp = RedisHelper.getCache(key, 10, () -> {
-                Emp tmp = new Emp();
-                tmp.setAddr("fj");
-                tmp.setAge(123);
-                tmp.setName(name);
-                tmp.setAddTime(LocalDateTime.now());
-                return tmp;
-            }, false);
-            Assert.assertEquals(emp.getName(), "xxx");
-            out(emp);
-            Thread.sleep(2000);
-        }
-    }
-
-    /**
-     * 从Redis反序列化的子类，必须是static的
-     */
-    @Data
-    static class Emp {
-        private String name;
-        private int age;
-        private String addr;
-        private LocalDateTime addTime;
+        super.TestCache();
     }
 }
